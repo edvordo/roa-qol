475f002e16cd25834d3dfb4b4b5c1e97923aa2ad
edvordo <edo@edvordo.sk>
2024-09-04 19:44:08 +0200

    recalculate info in header and swap labels

--- RoA-QoL.user.js
+++ RoA-QoL.user.js
@@ -9,7 +9,7 @@
 // @match        http://*.avabur.com/game*
 // @resource     QoLCSS             https://cdn.jsdelivr.net/gh/edvordo/roa-qol@2.8.6/resources/css/qol.css
 // @resource     QoLHeaderHTML      https://cdn.jsdelivr.net/gh/edvordo/roa-qol@2.8.4/resources/templates/header.html
-// @resource     QoLSettingsHTML    https://cdn.jsdelivr.net/gh/edvordo/roa-qol@2.8.4/resources/templates/settings.html
+// @resource     QoLSettingsHTML    https://cdn.jsdelivr.net/gh/edvordo/roa-qol@2.8.5/resources/templates/settings.html
 // @resource     SpectrumCSS        https://cdnjs.cloudflare.com/ajax/libs/spectrum/1.8.0/spectrum.min.css
 // @resource     favicon.ico        https://cdn.jsdelivr.net/gh/edvordo/roa-qol@2.8.8/resources/img/favicon.ico
 // @require      https://cdn.jsdelivr.net/gh/edvordo/roa-qol@2.8.7/common.js
@@ -611,6 +611,7 @@
                 updateStats(type, data) {
                     let now           = moment.tz(GAME_TIME_ZONE);
                     let hour          = 60 * 60 * 1000;
+                    let period        = hour * (true === VARIABLES.settings.gains_period_days ? 24 : 1);
                     let tmpl          = '<h5>Based upon</h5>{total} {label} over {count} {type} since {since}<h5>Would be gain / {period}</h5>{wannabe} / {period}';
                     let map           = {};
                     let count         = 0;
@@ -660,11 +661,11 @@
                             since  : trackingStart.format('Do MMM Y HH:mm:ss'),
                             type   : `${type} actions`,
                             period   : true === VARIABLES.settings.gains_period_days ? 'd' : 'h',
-                            wannabe: (Math.floor((hour * (true === VARIABLES.settings.gains_period_days ? 1 : 24)) / VARIABLES.QoLStats.na * map[e].c)).format(),
+                            wannabe: (Math.floor((period) / VARIABLES.QoLStats.na * map[e].c)).format(),
                         };
 
                         VARIABLES.QoLStats.e[e]
-                            .text((VARIABLES.QoLStats.d[ed] / (now - trackingStart) * hour).format())
+                            .text((VARIABLES.QoLStats.d[ed] / (now - trackingStart) * period).format())
                             .attr({'data-original-title': tmpl.formatQoL(obj)});
 
                     }
@@ -725,6 +726,12 @@
                         contentWrapper.insertAdjacentElement('afterend', chatWrapper);
                         navWrapper.insertAdjacentElement('afterend', contentWrapper);
                     }
+                },
+                swapLabelsForGains() {
+                    const period = true === VARIABLES.settings.gains_period_days ? '/ d:' : '/ h:';
+                    $('.rq-h > td.left')
+                      .filter((i,e) => /\/ [hd]:$/.test(e.textContent.trim()))
+                      .each((i, e) => e.textContent = e.textContent.replace(/\/ ([hd]):$/,  period));
                 },
                 colorGemOption(option) {
                     if (option.tagName !== 'OPTION') {
@@ -1546,6 +1553,7 @@
                     }
 
                     fn.helpers.chatContentSwap();
+                    fn.helpers.swapLabelsForGains();
 
                     document.querySelectorAll('.RQ-user-color-option').forEach(el => {
                         if (VARIABLES.settings.user_color_messages) {
