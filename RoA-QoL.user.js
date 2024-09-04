297bc75acf32e6aacc606756260389496d974e6b
edvordo <edo@edvordo.sk>
2024-09-04 19:53:13 +0200

    raise script version

--- RoA-QoL.user.js
+++ RoA-QoL.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RoA-QoL
 // @namespace    Reltorakii_is_awesome
-// @version      2.8.8
+// @version      2.8.9
 // @description  try to take over the world!
 // @author       Reltorakii
 // @icon         https://cdn.jsdelivr.net/gh/edvordo/roa-qol@2.8.4/resources/img/logo-32.png
